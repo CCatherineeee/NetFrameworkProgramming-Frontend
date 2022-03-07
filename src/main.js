16ceb0db293c76a5bd406b2ba779892eb2c31116
CCatherineeee <2870375520@qq.com>
2022-03-07 21:16:51 +0800

    login and register

--- src/main.js
+++ src/main.js
@@ -2,17 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-loadFonts()
+const app = createApp(App)
 
-createApp(App)
-    .use(router)
-    .use(store)
-    .use(vuetify)
-    .use(ElementPlus)
-    .mount('#app')
+app.use(ElementPlus).use(store).use(router)
+app.mount('#app')
